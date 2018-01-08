19f9ff194c66eb733c8587eb3c271274e6dfd0fb
Mawxder <mawxder@gmail.com>
2018-01-08 02:16:13 -0300

    first commit

---
+++ problem1.py
@@ -0,0 +1,6 @@
+# Find the sum of all the multiples of 3 or 5 below 1000
+storage = 0
+for element in range(1, 1000):
+    if (element%3 == 0) or (element%5 == 0):
+        storage = storage + element
+print(storage)
