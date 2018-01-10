cc27e614cdd51097c21e431671183553f5a5a1d9
Mawxder <mawxder@gmail.com>
2018-01-10 00:25:06 -0300

    update prob 3

--- problem3.py
+++ problem3.py
@@ -1,22 +1,25 @@
 # What is the largest prime factor of the number 600851475143
 
+# Set variables
 number = 600851475143
-
 primeList = []
 primeFactorList = []
+
+# Make list of prime numbers < 'number'
 for x in range(2, number+1):
     isPrime = True
+    # Don't calculate for more than the sqrt of number for efficiency
     for y in range(2, int(x**0.5)+1):
         if x % y == 0:
             isPrime = False
             break
     if isPrime:
         primeList.append(x)
-        print(x)
 
+# Iterate over primeList to check for prime factors of 'number'
 for i in primeList:
     if number % i == 0:
         primeFactorList.append(i)
-        print(i)
-
+        
+# Print largest prime factor of number
 print(max(primeFactorList))
