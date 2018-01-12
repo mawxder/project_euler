f8a35dd7f72b136dbb2c924bfda2516bd4468285
Mawxder <mawxder@gmail.com>
2018-01-11 21:45:33 -0300

    Largest prime factor

--- problem3.py
+++ problem3.py
@@ -1,25 +1,32 @@
 # What is the largest prime factor of the number 600851475143
+import time
+t1 = time.clock()
 
 # Set variables
 number = 600851475143
-primeList = []
 primeFactorList = []
 
-# Make list of prime numbers < 'number'
-for x in range(2, number+1):
-    isPrime = True
-    # Don't calculate for more than the sqrt of number for efficiency
-    for y in range(2, int(x**0.5)+1):
-        if x % y == 0:
-            isPrime = False
-            break
-    if isPrime:
-        primeList.append(x)
+def primeList(number):
+    # Make list of prime numbers < 'number'
+    for x in range(2, number+1):
+        isPrime = True
+        # Don't calculate for more than the sqrt of number for efficiency
+        for y in range(2, int(x**0.5)+1):
+            if x % y == 0:
+                isPrime = False
+                break
+        if isPrime:
+            yield x
 
-# Iterate over primeList to check for prime factors of 'number'
-for i in primeList:
+# Calculate  primes using primeList and check for prime factors of 'number'
+for i in primeList(number):
+    if i > number**0.5:
+        break
     if number % i == 0:
         primeFactorList.append(i)
 
 # Print largest prime factor of 'number'
 print(max(primeFactorList))
+t2 = time.clock()
+
+print('Took {} seconds'.format(t2-t1))
