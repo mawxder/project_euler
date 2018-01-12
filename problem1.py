dcb17a5bce90e1b53dfcc9516b89e95977793e1f
Mawxder <mawxder@gmail.com>
2018-01-11 21:57:35 -0300

    Multiples of 3 and 5

--- problem1.py
+++ problem1.py
@@ -1,6 +1,6 @@
-# Find the sum of all the multiples of 3 or 5 below 1000
+# Find the sum of all the multiples of 3 or 5 below 1000.
 storage = 0
-for element in range(1, 1000):
+for element in range(1, 1000 + 1):
     if (element%3 == 0) or (element%5 == 0):
         storage = storage + element
 print(storage)
